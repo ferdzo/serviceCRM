37db8f69ad4cb0a6041e18490efcd7341a2ea245
ferdzo <ferdzo@users.noreply.github.com>
2024-03-15 00:33:03 +0100

    tset

--- serviceCRM/tables.py
+++ serviceCRM/tables.py
@@ -1,10 +1,13 @@
 import django_tables2 as tables
 from .models import Insert
+from django_tables2.utils import A
 
 
 class InsertTable(tables.Table):
     class Meta:
         model = Insert
-        template_name = "serviceCRM/list.html"
-        edit = tables.TemplateColumn(template_name="serviceCRM/edit.html", verbose_name="edit", orderable=False)
-
+        template_name = "list.html"
+        sequence = ("name", "phone", "description","date","done" )
+        delete = tables.LinkColumn('main:delete_item', args=[A('pk')], attrs={
+            'a': {'class': 'btn'}
+        })
